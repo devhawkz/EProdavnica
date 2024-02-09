fa2270b5a1f387eb61af216e9091798416ededd7
devhawkz <skydevhawkz@gmail.com>
2024-02-09 08:49:20 +0100

    Dodali smo novi kontroler KategorijaController i kreirali metodu GetKategorije() u njemu.

---
+++ EProdavnica/Server/Controllers/KategorijaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EProdavnica.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class KategorijaController : ControllerBase
+{
+    private readonly IKategorijaService _kategorijaService;
+
+    public KategorijaController(IKategorijaService kategorijaService)
+    {
+        _kategorijaService = kategorijaService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult <ServiceResponse<List<Kategorija>>>> GetKategorije()
+    {
+        var rezultat = await _kategorijaService.GetKategorijeAsync();
+        return Ok(rezultat);
+    }
+}
