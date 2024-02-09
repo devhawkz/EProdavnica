fd13fd47d1c9a2f4a870f07953833a6e18cda853
devhawkz <skydevhawkz@gmail.com>
2024-02-09 10:15:02 +0100

    Dodali smo novu metodu GetProizvodiyKategorijaAsync(string kategorijaUrl) u interfejsu IProizvodService i implementirali je u ProizvodService. Kreirali smo novu metodu GetProizvodiyKategorija(string kategorijaUrl) u kontroleru ProizvodController i u njoj pozvali gore navedenu metodu.

--- EProdavnica/Server/Services/ProductService/IProizvodService.cs
+++ EProdavnica/Server/Services/ProductService/IProizvodService.cs
@@ -4,4 +4,5 @@ public interface IProizvodService
 {
     Task<ServiceResponse<List<Proizvod>>> GetProizvodiAsync();
     Task<ServiceResponse<Proizvod>> GetProizvodAsync(int proizvodId);
+    Task<ServiceResponse<List<Proizvod>>> GetProizvodiByKategorijaAsync(string kategorijaUrl);
 }
