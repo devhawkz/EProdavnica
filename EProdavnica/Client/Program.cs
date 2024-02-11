abb901b2c91544c6e8adf893f7ddfec75eef5469
devhawkz <skydevhawkz@gmail.com>
2024-02-11 23:23:54 +0100

    Dodali smo NuGet paket Blazored.LocalStorage u klijent projekat Dodali smo novi middleware u Program.cs klijent projetka Dodali smo Blazored.LocalStorage u imports fajl.

--- EProdavnica/Client/Program.cs
+++ EProdavnica/Client/Program.cs
@@ -4,6 +4,7 @@ global using EProdavnica.Client.Services.ProductService;
 global using EProdavnica.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 
 namespace EProdavnica.Client
@@ -13,11 +14,17 @@ namespace EProdavnica.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
             builder.RootComponents.Add<App>("#app");
+            
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
+            
+            builder.Services.AddBlazoredLocalStorage();
+            
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
             builder.Services.AddScoped<IProizvodService, ProizvodService>();
+            
             builder.Services.AddScoped<IKategorijaService, KategorijaService>();
 
             await builder.Build().RunAsync();
