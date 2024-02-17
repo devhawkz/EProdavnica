78e0a02ea8eddbf461f74f0bcd7ef05df1095e35
devhawkz <skydevhawkz@gmail.com>
2024-02-17 12:19:13 +0100

    Dodali smo novu model klasu KorisnickaRegistracija. Dodali smo novu komponentu Registracija.razor u pages folderu Dodali smo novu metodu u komponentu UpravljanjeRegistracijom() koja se pokrece samo kada je registracija korisnika uspesno izvrsena (Svi uneti podaci su ispravno uneti i u ispravnom su formatu)
    Dodali smo novu komponentu DugmeKorisnika u shared folder klijent projekta
    U njoj smo dodali ikonicu korisnika, dugme koje nas vodi na komponentu Registracija.razor.
    Dodali smo fleg prikaziMeniKorisnika i 2 metode  UpravljanjeMenijemKorisnika() i HideUserMenu()
    U komponenti ProdavnicaLayout smo pozvali komponentu DugmeKorisnika.

--- EProdavnica/Shared/ProizvodUKorpi.cs
+++ EProdavnica/Shared/ProizvodUKorpi.cs
@@ -7,5 +7,5 @@ public class ProizvodUKorpi
     // na osnovu ova 2 svojstva mozemo sve ostale podatke o proizvodu da dobijemo sa servera
     public int ProizvodId {  get; set; }
     public int TipProizvodaId { get; set; }
-    public int Kolicina {  get; set; }
+    public int Kolicina { get; set; } = 1;
 }
---
+++ EProdavnica/Shared/RegistracijaKorisnika.cs
@@ -0,0 +1,10 @@
+﻿namespace EProdavnica.Shared;
+
+public class RegistracijaKorisnika
+{
+    public string Email { get; set; } = string.Empty;
+
+    public string Lozinka { get; set; } = string.Empty;
+
+    public string PotvrdaLozinke { get; set; } = string.Empty;
+}
