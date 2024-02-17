60255285f223b2ddfd74ee006b8cf8b11a45058d
devhawkz <skydevhawkz@gmail.com>
2024-02-17 13:12:31 +0100

    Dodali smo Data Annotations (atribute) u RegistracijaKorisnika model klasi kako bi validairali unete podatke. Aktivirali smo validaciju u Registracija.razor komponenti sa 2 taga <DataAnnotationsValidator /> i <ValidationSummary />

--- EProdavnica/Shared/RegistracijaKorisnika.cs
+++ EProdavnica/Shared/RegistracijaKorisnika.cs
@@ -1,10 +1,15 @@
-﻿namespace EProdavnica.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EProdavnica.Shared;
 
 public class RegistracijaKorisnika
 {
+    [Required, EmailAddress] //obavezno polje i vrsi se provera da li je uneti mejl u ispravnom formatu
     public string Email { get; set; } = string.Empty;
 
+    [Required, StringLength(100, MinimumLength = 6)] // obavezno polje max duzina je 100 a min 6 lozinke 
     public string Lozinka { get; set; } = string.Empty;
 
+    [Compare("Lozinka", ErrorMessage = "Lozinke se ne podudaraju")] // uporedjuje vrednosti polja Lozinka i Potvrda lozinke, ako se ne podudaraju prikazuje se error message (opciono).
     public string PotvrdaLozinke { get; set; } = string.Empty;
 }
